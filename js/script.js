f2c061df1d5fd1d4ea7268607e3ced689f1c3b78
Rafael Maito <rafaelmaito@hotmail.com>
2020-11-04 14:30:14 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+window.onload = function () {
+  rememberCards();
+};
+const cardsBoard = document.getElementById('cardsBoard');
+
+const realCards = [
+  'car-topview.svg',
+  'car-topview-2.svg',
+  'car-topview-3.svg',
+  'car-topview-4.svg',
+  'car-topview-5.svg',
+  'car-topview-6.svg',
+];
+let firstCard,
+  secondCard,
+  moves = 0,
+  points = 0;
+let pointsInput = document.getElementById('pointsInput'),
+  movesInput = document.getElementById('movesInput');
+function showCards() {
+  cardsBoard.innerHTML = '';
+  shuffleCards();
+  shuffleCards();
+}
+showCards();
+
+function shuffleCards() {
+  realCards.sort(() => Math.random() - 0.5);
+  for (const item of realCards) {
+    cardsBoard.innerHTML += `
+      <div class="cardMem d-flex align-items-center justify-content-center flex-wrap" onclick="flipCard(event)">
+      <img src="./img/${item}" />
+      <img src="./img/inte.svg"  />
+      </div>
+        `;
+  }
+}
+
+function rememberCards() {
+  const cards = document.querySelectorAll('.cardMem');
+  cards.forEach((card) => {
+    card.classList.add('flip');
+  });
+  setTimeout(() => {
+    cards.forEach((card) => {
+      card.classList.remove('flip');
+    });
+  }, 2000);
+}
+
+function compareCards(firstCard, secondCard) {
+  moves++;
+  movesInput.value = moves;
+  if (firstCard.outerHTML === secondCard.outerHTML) {
+    firstCard.removeAttribute('onclick');
+    secondCard.removeAttribute('onclick');
+    points++;
+    pointsInput.value = points;
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      firstCard.setAttribute('onclick', 'flipCard(event)');
+      secondCard.setAttribute('onclick', 'flipCard(event)');
+    }, 1000);
+  }
+  if (points === 6) {
+    alert(`Parabéns você venceu`);
+  }
+}
+function flipCard(event) {
+  const cardFunc = event.path[1];
+
+  if (!firstCard) {
+    firstCard = cardFunc;
+    firstCard.removeAttribute('onclick');
+    firstCard.classList.add('flip');
+    return;
+  }
+  if (!secondCard) {
+    secondCard = cardFunc;
+    secondCard.removeAttribute('onclick');
+    secondCard.classList.add('flip');
+    compareCards(firstCard, secondCard);
+  }
+  firstCard = undefined;
+  secondCard = undefined;
+  return;
+}
